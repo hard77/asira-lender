ab92abd41d0b34b2d8592cba514048dc4e9d8644
Christian <cwinata@ayannah.com>
2019-09-03 16:53:32 +0700

    balikin migration borrower dan loan

--- migration/migration.go
+++ migration/migration.go
@@ -3,11 +3,13 @@ package migration
 import (
 	"asira_lender/asira"
 	"asira_lender/models"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -159,6 +161,232 @@ func Seed() {
 		}
 		for _, lender := range lenders {
 			lender.Create()
+		}
+
+		// @ToDo borrower and loans should be get from borrower platform
+		// seed borrowers
+		borrowers := []models.Borrower{
+			models.Borrower{
+				Fullname:             "Full Name A",
+				Gender:               "M",
+				IdCardNumber:         "9876123451234567789",
+				TaxIDnumber:          "0987654321234567890",
+				Email:                "emaila@domain.com",
+				Birthday:             time.Now(),
+				Birthplace:           "a birthplace",
+				LastEducation:        "a last edu",
+				MotherName:           "a mom",
+				Phone:                "081234567890",
+				MarriedStatus:        "single",
+				SpouseName:           "a spouse",
+				SpouseBirthday:       time.Now(),
+				SpouseLastEducation:  "master",
+				Dependants:           0,
+				Address:              "a street address",
+				Province:             "a province",
+				City:                 "a city",
+				NeighbourAssociation: "a rt",
+				Hamlets:              "a rw",
+				HomePhoneNumber:      "021837163",
+				Subdistrict:          "a camat",
+				UrbanVillage:         "a lurah",
+				HomeOwnership:        "privately owned",
+				LivedFor:             5,
+				Occupation:           "accupation",
+				EmployerName:         "amployer",
+				EmployerAddress:      "amployer address",
+				Department:           "a department",
+				BeenWorkingFor:       2,
+				DirectSuperior:       "a boss",
+				EmployerNumber:       "02188776655",
+				MonthlyIncome:        5000000,
+				OtherIncome:          2000000,
+				RelatedPersonName:    "a big sis",
+				RelatedPhoneNumber:   "08987654321",
+				OTPverified:          true,
+				BankAccountNumber:    "520384716",
+				Password:             "password",
+				Bank: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+			},
+			models.Borrower{
+				Fullname:             "Full Name B",
+				Gender:               "F",
+				IdCardNumber:         "9876123451234567781",
+				TaxIDnumber:          "0987654321234567891",
+				Email:                "emailb@domain.com",
+				Birthday:             time.Now(),
+				Birthplace:           "b birthplace",
+				LastEducation:        "b last edu",
+				MotherName:           "b mom",
+				Phone:                "081234567891",
+				MarriedStatus:        "single",
+				SpouseName:           "b spouse",
+				SpouseBirthday:       time.Now(),
+				SpouseLastEducation:  "master",
+				Dependants:           0,
+				Address:              "b street address",
+				Province:             "b province",
+				City:                 "b city",
+				NeighbourAssociation: "b rt",
+				Hamlets:              "b rw",
+				HomePhoneNumber:      "021837163",
+				Subdistrict:          "b camat",
+				UrbanVillage:         "b lurah",
+				HomeOwnership:        "privately owned",
+				LivedFor:             5,
+				Occupation:           "bccupation",
+				EmployerName:         "bmployer",
+				EmployerAddress:      "bmployer address",
+				Department:           "b department",
+				BeenWorkingFor:       2,
+				DirectSuperior:       "b boss",
+				EmployerNumber:       "02188776655",
+				MonthlyIncome:        5000000,
+				OtherIncome:          2000000,
+				RelatedPersonName:    "b big sis",
+				RelatedPhoneNumber:   "08987654321",
+				RelatedAddress:       "big sis address",
+				OTPverified:          false,
+				Password:             "password",
+				Bank: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+			},
+		}
+		for _, borrower := range borrowers {
+			borrower.Create()
+		}
+
+		// seed loans
+		fees := []models.LoanFee{
+			models.LoanFee{
+				Description: "fee 1",
+				Amount:      1000,
+			},
+		}
+		jMarshal, _ = json.Marshal(fees)
+		loans := []models.Loan{
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name A",
+				LoanAmount:       5000000,
+				Installment:      8,
+				LoanIntention:    "a loan 1 intention",
+				IntentionDetails: "a loan 1 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(6500000),
+				LayawayPlan:      500000,
+			},
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 2,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name B",
+				LoanAmount:       2000000,
+				Installment:      3,
+				LoanIntention:    "a loan 1 intention",
+				IntentionDetails: "a loan 1 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(3000000),
+				LayawayPlan:      200000,
+			},
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name C",
+				LoanAmount:       29000000,
+				Installment:      3,
+				LoanIntention:    "a loan 1 intention",
+				IntentionDetails: "a loan 1 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(6500000),
+				LayawayPlan:      500000,
+			},
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 2,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name D",
+				LoanAmount:       3000000,
+				Installment:      3,
+				LoanIntention:    "a loan 1 intention",
+				IntentionDetails: "a loan 1 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(3000000),
+				LayawayPlan:      200000,
+			},
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 2,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name E",
+				LoanAmount:       9123456,
+				Installment:      3,
+				LoanIntention:    "a loan 3 intention",
+				IntentionDetails: "a loan 5 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(3000000),
+				LayawayPlan:      200000,
+			},
+			models.Loan{
+				Bank: sql.NullInt64{
+					Int64: 2,
+					Valid: true,
+				},
+				Owner: sql.NullInt64{
+					Int64: 1,
+					Valid: true,
+				},
+				OwnerName:        "Full Name F",
+				LoanAmount:       80123456,
+				Installment:      11,
+				LoanIntention:    "a loan 3 intention",
+				IntentionDetails: "a loan 5 intention details",
+				Fees:             postgres.Jsonb{jMarshal},
+				Interest:         1.5,
+				TotalLoan:        float64(3000000),
+				LayawayPlan:      200000,
+			},
+		}
+		for _, loan := range loans {
+			loan.Create()
 		}
 	}
 }
