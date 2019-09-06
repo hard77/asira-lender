d175a50f16823e5bc7b20df2a202ee01fbd1cdf1
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-09-06 16:26:46 +0700

    create unit test

---
+++ tests/lender_borrower_list_test.go
@@ -0,0 +1,44 @@
+package tests
+
+import (
+	"asira_lender/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gavv/httpexpect"
+)
+
+func TestBorrowerGetAll(t *testing.T) {
+	RebuildData()
+
+	api := router.NewRouter()
+
+	server := httptest.NewServer(api)
+
+	defer server.Close()
+
+	e := httpexpect.New(t, server.URL)
+
+	auth := e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Basic "+clientBasicToken)
+	})
+
+	lendertoken := getLenderLoginToken(e, auth, "1")
+
+	auth = e.Builder(func(req *httpexpect.Request) {
+		req.WithHeader("Authorization", "Bearer "+lendertoken)
+	})
+
+	// valid response of borrowers
+	obj := auth.GET("/lender/borrower_list").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+	obj.ContainsKey("to").ValueEqual("to", 25)
+
+	// valid response of borrowers
+	obj = auth.GET("/lender/borrower_list").WithQuery("fullname", "ame").
+		Expect().
+		Status(http.StatusOK).JSON().Object()
+	obj.ContainsKey("to").ValueEqual("to", 25)
+}
