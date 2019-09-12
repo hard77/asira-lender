7c9a5da89187b0fc79efa5a0fb58fb7625a8b2d9
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-09-12 11:05:11 +0700

    update get all and details internal roles

---
+++ admin_handlers/roles.go
@@ -0,0 +1,43 @@
+package admin_handlers
+
+import (
+	"asira_lender/models"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+func GetAllRole(c echo.Context) error {
+	defer c.Request().Body.Close()
+
+	Iroles := models.InternalRoles{}
+	// pagination parameters
+	rows, err := strconv.Atoi(c.QueryParam("rows"))
+	page, err := strconv.Atoi(c.QueryParam("page"))
+	orderby := c.QueryParam("orderby")
+	sort := c.QueryParam("sort")
+
+	var Filter struct{}
+	result, err := Iroles.PagedFilterSearch(page, rows, orderby, sort, &Filter)
+
+	if err != nil {
+		return returnInvalidResponse(http.StatusNotFound, err, "Internal Role tidak Ditemukan")
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func RoleGetDetails(c echo.Context) error {
+	defer c.Request().Body.Close()
+
+	Iroles := models.InternalRoles{}
+
+	IrolesID, _ := strconv.Atoi(c.Param("role_id"))
+	_, err := Iroles.FindbyID(IrolesID)
+	if err != nil {
+		return returnInvalidResponse(http.StatusNotFound, err, "Role ID tidak ditemukan")
+	}
+
+	return c.JSON(http.StatusOK, Iroles)
+}
