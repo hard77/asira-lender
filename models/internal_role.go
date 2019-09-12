5baeb46825b6d4d326d8c8da6869e7455c939b7f
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-09-12 16:46:07 +0700

    Update internal_role.go

--- models/internal_role.go
+++ models/internal_role.go
@@ -29,3 +29,15 @@ func (b *InternalRoles) FindbyID(id int) (*InternalRoles, error) {
 	err := FindbyID(&b, id)
 	return b, err
 }
+
+func (b *InternalRoles) FilterSearchSingle(filter interface{}) (*InternalRoles, error) {
+	err := FilterSearchSingle(&b, filter)
+	return b, err
+}
+
+func (b *InternalRoles) PagedFilterSearch(page int, rows int, orderby string, sort string, filter interface{}) (result PagedSearchResult, err error) {
+	internal := []InternalRoles{}
+	result, err = PagedFilterSearch(&internal, page, rows, orderby, sort, filter)
+
+	return result, err
+}
