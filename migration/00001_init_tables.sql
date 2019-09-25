2dde6395b419182901f2f704064d8a2fabc70b49
Christian <cwinata@ayannah.com>
2019-09-25 13:56:10 +0700

    bank db and model

--- migration/00001_init_tables.sql
+++ migration/00001_init_tables.sql
@@ -36,7 +36,7 @@ CREATE TABLE "banks" (
     "updated_time" timestamptz DEFAULT CURRENT_TIMESTAMP,
     "deleted_time" timestamptz,
     "name" varchar(255),
-    "type" bigint,
+    "type" bigserial,
     "address" text,
     "province" varchar(255),
     "city" varchar(255),
