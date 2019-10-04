a7e4e2da6893f12a5dfa3fe8ca2975d23ac799c7
Christian <cwinata@ayannah.com>
2019-10-04 11:22:48 +0700

    add created_time

--- reports/convenienceFee_report.go
+++ reports/convenienceFee_report.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"gitlab.com/asira-ayannah/basemodel"
 
@@ -19,11 +20,12 @@ func ConvenienceFeeReport(c echo.Context) error {
 	db := asira.App.DB
 
 	type ConvenienceFeeReport struct {
-		BankName       string  `json:"bank_name"`
-		ServiceName    string  `json:"service_name"`
-		LoanID         string  `json:"loan_id"`
-		Plafond        float64 `json:"plafond"`
-		ConvenienceFee float64 `json:"convenience_fee"`
+		BankName       string    `json:"bank_name"`
+		ServiceName    string    `json:"service_name"`
+		LoanID         string    `json:"loan_id"`
+		CreatedTime    time.Time `json:"created_time"`
+		Plafond        float64   `json:"plafond"`
+		ConvenienceFee float64   `json:"convenience_fee"`
 	}
 	var results []ConvenienceFeeReport
 	var totalRows int
@@ -40,7 +42,7 @@ func ConvenienceFeeReport(c echo.Context) error {
 	offset := (page * rows) - rows
 
 	db = db.Table("loans l").
-		Select("b.name as bank_name, ss.name as service_name, l.id as loan_id, loan_amount as plafond, value->>'amount' as convenience_fee").
+		Select("b.name as bank_name, ss.name as service_name, l.id as loan_id, l.created_time, loan_amount as plafond, value->>'amount' as convenience_fee").
 		Joins("JOIN LATERAL jsonb_array_elements(l.fees) j ON true").
 		Joins("INNER JOIN banks b ON b.id = l.bank").
 		Joins("INNER JOIN bank_products p ON p.id = l.product").
@@ -63,6 +65,13 @@ func ConvenienceFeeReport(c echo.Context) error {
 	}
 	if convenienceFee := c.QueryParam("convenience_fee"); len(convenienceFee) > 0 {
 		db = db.Where("value->>'amount' = ?", convenienceFee)
+	}
+	if startDate := c.QueryParam("start_date"); len(startDate) > 0 {
+		endDate := c.QueryParam("end_date")
+		if len(endDate) < 1 {
+			endDate = startDate
+		}
+		db = db.Where("l.created_time BETWEEN ? AND ?", startDate, endDate)
 	}
 
 	err := db.Limit(rows).Offset(offset).
